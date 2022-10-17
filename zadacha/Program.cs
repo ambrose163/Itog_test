3e5f6ff8487ec01a0e37cb78459538f346412032
Dmitriy <ambrose163@mail.ru>
2022-10-17 10:05:02 +0400

    save4

---
+++ zadacha/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+Console.Clear();
+
+string[] stringArray = {"hello", "2", "world", "!", "-65", "Kazan", "and", "computer science", ":)"};
+
+PrintNewArray(stringArray);
+
+void PrintNewArray(string[] array)
+{
+    string[] newArray = Array.FindAll(array, item => item.Length <=3);
+
+    for (int idx = 0; idx < newArray.Length; idx++)
+    {
+        if (idx != newArray.Length - 1)
+            Console.Write($"{newArray[idx]}, ");
+        else
+            Console.WriteLine($"{newArray[idx]}");
+    }
+}
